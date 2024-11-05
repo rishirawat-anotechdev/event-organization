b9004cd1ea15890a612f4bd9f4cc6a608c5e8b03
rishirawat-anotechdev <rishi.rawat@anotechsolutions.com>
2024-11-05 12:27:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ if (cluster.isPrimary) {
 
   // Enable CORS for requests from localhost:3000
   app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://event-organization-tm99.onrender.com",
     credentials: true, // Allow sending cookies from frontend
   }));
 
